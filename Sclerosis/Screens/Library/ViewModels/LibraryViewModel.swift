8f7be55d366fe7815c35368e191055009659e147
Danil Kazakov <dani4upa586@gmail.com>
2025-09-24 17:39:46 +0900

    feat(viewmodel): add @Observable LibraryViewModel with mocked data

---
+++ Sclerosis/Screens/Library/ViewModels/LibraryViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  LibraryViewModel.swift
+//  Sclerosis
+//
+//  Created by Danil Kazakov on 22.09.2025.
+//
+
+import Observation
+
+@MainActor
+@Observable
+final class LibraryViewModel {
+    var shows = Show.MockShows
+}
