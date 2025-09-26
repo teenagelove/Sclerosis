4281150e9dcb2797850969fb14c0dfa2c6ce5d5a
Danil Kazakov <dani4upa586@gmail.com>
2025-09-26 21:34:25 +0900

    feat: implement LoadingView with SwiftfulLoadingIndicators
    - Replace placeholder text with LoadingIndicator animation
    - Use fiveLinesPulse animation for loading state
    - Import SwiftfulLoadingIndicators library

--- Sclerosis/Screens/Components/Views/LoadingView.swift
+++ Sclerosis/Screens/Components/Views/LoadingView.swift
@@ -5,11 +5,12 @@
 //  Created by Danil Kazakov on 26.09.2025.
 //
 
+import SwiftfulLoadingIndicators
 import SwiftUI
 
 struct LoadingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        LoadingIndicator(animation: .fiveLinesPulse)
     }
 }
 
