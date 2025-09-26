05efa2544ef7bc0d692aa7325989af661cbba1c4
Danil Kazakov <dani4upa586@gmail.com>
2025-09-26 21:33:50 +0900

    feat: add LoadingView component template
    - Add basic LoadingView.swift template
    - Prepare component structure for loading states implementation

---
+++ Sclerosis/Screens/Components/Views/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  Sclerosis
+//
+//  Created by Danil Kazakov on 26.09.2025.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
