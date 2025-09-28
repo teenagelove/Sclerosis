964487e12ff1991d25f471a7a5f51ba7bbe6078a
Danil Kazakov <dani4upa586@gmail.com>
2025-09-28 17:02:39 +0900

    feat: create CalendarView component template
    - Add basic CalendarView.swift template in Calendar/Views directory
    - Prepare structure for calendar screen implementation
    - Include standard SwiftUI boilerplate with Preview

---
+++ Sclerosis/Screens/Calendar/Views/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarView.swift
+//  Sclerosis
+//
+//  Created by Danil Kazakov on 28.09.2025.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}
