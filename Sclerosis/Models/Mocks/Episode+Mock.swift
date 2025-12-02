51bf02c3d2db7b9953f6336d7849871d402a7c9d
Danil Kazakov <dani4upa586@gmail.com>
2025-12-02 21:54:59 +0900

    refactor(mocks): update mock data for episode and show models

--- Sclerosis/Models/Mocks/Episode+Mock.swift
+++ Sclerosis/Models/Mocks/Episode+Mock.swift
@@ -11,6 +11,60 @@ extension Episode {
     private static let calendar = Calendar.current
     private static let today = calendar.startOfDay(for: Date()) // Игнорируем время, чтобы даты были только днями
 
+    // MARK: - Individual Episode Mocks
+    static let bbPilotEpisode = Episode(
+        name: "Пилот",
+        showName: "Во все тяжкие",
+        releaseDate: makeDateFromToday(addingDays: 1)
+    )
+    static let bbWalletOrLifeEpisode = Episode(
+        name: "Кошелек или жизнь",
+        showName: "Во все тяжкие",
+        releaseDate: makeDateFromToday(addingDays: 2)
+    )
+    static let bbSevenThirtySevenEpisode = Episode(
+        name: "Семь тридцать семь",
+        showName: "Во все тяжкие",
+        releaseDate: makeDateFromToday(addingDays: 3)
+    )
+    static let bbGrippyEpisode = Episode(
+        name: "Гриппозный",
+        showName: "Во все тяжкие",
+        releaseDate: makeDate(2009, 3, 15)
+    )
+
+    static let gotWinterIsComingEpisode = Episode(
+        name: "Зима близко",
+        showName: "Игра престолов",
+        releaseDate: makeDate(2011, 4, 17)
+    )
+    static let gotKingsroadEpisode = Episode(
+        name: "Дорога королей",
+        showName: "Игра престолов",
+        releaseDate: makeDate(2011, 4, 24)
+    )
+
+    static let chernobylOneTwentyThreeFortyFiveEpisode = Episode(
+        name: "1:23:45",
+        showName: "Чернобыль",
+        releaseDate: makeDate(2019, 5, 6)
+    )
+    static let chernobylStayCalmEpisode = Episode(
+        name: "Пожалуйста, оставайтесь спокойны",
+        showName: "Чернобыль",
+        releaseDate: makeDate(2019, 5, 13)
+    )
+
+    init(name: String, showName: String, releaseDate: Date) {
+        self.init(
+            id: String(Int.random(in: 1...100000)),
+            name: name,
+            showName: showName,
+            showURL: nil,
+            releaseDate: releaseDate
+        )
+    }
+
     /// Вспомогательная функция для создания даты из компонентов (для старых, фиксированных дат)
     private static func makeDate(_ year: Int, _ month: Int, _ day: Int) -> Date {
         var comps = DateComponents()
@@ -24,18 +78,6 @@ extension Episode {
     private static func makeDateFromToday(addingDays days: Int) -> Date {
         calendar.date(byAdding: .day, value: days, to: today) ?? today
     }
-
-    // MARK: - Individual Episode Mocks
-    static let bbPilotEpisode = Episode(name: "Пилот", showName: "Во все тяжкие", releaseDate: makeDateFromToday(addingDays: 1))
-    static let bbWalletOrLifeEpisode = Episode(name: "Кошелек или жизнь", showName: "Во все тяжкие", releaseDate: makeDateFromToday(addingDays: 2))
-    static let bbSevenThirtySevenEpisode = Episode(name: "Семь тридцать семь", showName: "Во все тяжкие", releaseDate: makeDateFromToday(addingDays: 3))
-    static let bbGrippyEpisode = Episode(name: "Гриппозный", showName: "Во все тяжкие", releaseDate: makeDate(2009, 3, 15))
-
-    static let gotWinterIsComingEpisode = Episode(name: "Зима близко", showName: "Игра престолов", releaseDate: makeDate(2011, 4, 17))
-    static let gotKingsroadEpisode = Episode(name: "Дорога королей", showName: "Игра престолов", releaseDate: makeDate(2011, 4, 24))
-
-    static let chernobylOneTwentyThreeFortyFiveEpisode = Episode(name: "1:23:45", showName: "Чернобыль", releaseDate: makeDate(2019, 5, 6))
-    static let chernobylStayCalmEpisode = Episode(name: "Пожалуйста, оставайтесь спокойны", showName: "Чернобыль", releaseDate: makeDate(2019, 5, 13))
 }
 
 extension Episode {
--- Sclerosis/Models/Mocks/Show+Mock.swift
+++ Sclerosis/Models/Mocks/Show+Mock.swift
@@ -12,25 +12,28 @@ extension Show {
     static let MockShows: [Show] = {
         return [
             Show(
+                id: "1",
                 name: "Во все тяжкие",
                 description: "Учитель химии становится варщиком метамфетамина, чтобы обеспечить семью.",
-                cover: URL(string: "https://static.tvmaze.com/uploads/images/medium_portrait/501/1253519.jpg")!,
-                seasons: [Season.bbSeason1Mock, Season.bbSeason2Mock]
+                cover: URL(string: "https://static.tvmaze.com/uploads/images/medium_portrait/501/1253519.jpg"),
+//                seasons: [Season.bbSeason1Mock, Season.bbSeason2Mock]
             ),
             Show(
+                id: "2",
                 name: "Игра престолов",
                 description: "Битва за Железный трон и судьбу Семи Королевств.",
-                cover: URL(string: "https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/dd78edfd-6a1f-486c-9a86-6acbca940418/600x900")!,
-                seasons: [Season.gotSeason1Mock]
+                cover: URL(string: "https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/dd78edfd-6a1f-486c-9a86-6acbca940418/600x900"),
+//                seasons: [Season.gotSeason1Mock]
             ),
             Show(
+                id: "3",
                 name: "Чернобыль",
                 description: "Драматизация событий вокруг аварии на ЧАЭС 1986 года.",
-                cover: URL(string: "https://avatars.mds.yandex.net/get-kinopoisk-image/1600647/3d0b4a89-acd9-49ac-a83e-480c18c41baa/600x900")!,
-                seasons: [Season.chernobylSeason1Mock]
+                cover: URL(string: "https://avatars.mds.yandex.net/get-kinopoisk-image/1600647/3d0b4a89-acd9-49ac-a83e-480c18c41baa/600x900"),
+//                seasons: [Season.chernobylSeason1Mock]
             )
         ]
     }()
 
-    static let MockShow = MockShows.first!
+    static let MockShow = MockShows[0]
 }
